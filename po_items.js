d6fbb8304d7cecb6e327a93c7115ebc116f5fc39
Phant01 <81266097+Phant01@users.noreply.github.com>
2026-04-30 09:20:46 +0800

    Delete po_items.js

--- po_items.js
+++
@@ -1,8 +0,0 @@
-// PO-specific item row logic (extends app.js)
-$(function() {
-  $('#btn-add-row').on('click', function() {
-    if (typeof addItemRow === 'function') {
-      addItemRow('po-items-table', window.ITEMS_DATA || []);
-    }
-  });
-});
